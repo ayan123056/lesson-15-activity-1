ba2c58d14c6725df2802acaab8a356c24193fced
ayan123056 <jasirafowaz@yahoo.com>
2024-12-11 20:35:48 +0400

    YES

---
+++ py.py
@@ -0,0 +1,6 @@
+def weather_condition():
+    print("The weather is plesant in:", spring)
+    print("the weather is same in", autumn)
+spring = "IS THE WEATHER NICE IN AUTUMN OR IS IT SPRING OR IS IT SUMMER OR IS IT EVEN DUH DUH WINTER"
+autumn = spring
+weather_condition()
